a99e9cb5b03f9de2b90b61040897b8aba11f32ba
deepayan (Deepayan Sarkar) <deepayan.sarkar@gmail.com>
2009-11-09 19:32:59 +0000

    updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qtinterfaces@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qtutils/R/modalutils.R
+++ pkg/qtutils/R/modalutils.R
@@ -10,21 +10,91 @@ qfile.choose <-
     FUN <-
         if (allow.new) Qt$QFileDialog$getSaveFileName
         else Qt$QFileDialog$getOpenFileName
-    ans <- FUN(parent, caption, path.expand(dir), filter)
+    ans <- FUN(parent, as.character(caption),
+               path.expand(dir), as.character(filter))
     (ans)
 }
 
 qdir.choose <- function(caption = "", dir = "", parent = NULL)
 {
-
-
-    .Call(qt_qdir_choose,
-          as.character(caption[1]),
-          as.character(dir[1]),
-          if (is(parent, "QWidget")) parent else NULL)
+    FUN <- Qt$QFileDialog$getExistingDirectory
+    ans <- FUN(parent,
+               as.character(caption),
+               as.character(dir))
+    (ans)
 }
 
-qexport <- function(x, ...) UseMethod("qexport")
+qgetColor <- function(parent = NULL, title = "",
+                      alpha = 1)
+{
+    ok <- TRUE
+    FUN <- Qt$QColorDialog$getColor
+    ans <- FUN(Qt$QColor(255, 255, 255),
+               parent,
+               as.character(title))
+    if (ans$isValid())
+        rgb(ans$red(), ans$green(), ans$blue(),
+            alpha * 255, maxColorValue = 255)
+    else NA_character_
+}
+
+qgetDouble <-
+    function(title = "Choose value",
+             label = "Enter a numeric value",
+             value = 0.0,
+             minValue = -.Machine$double.xmax,
+             maxValue = .Machine$double.xmax,
+             decimals = 3L, parent = NULL)
+{
+    FUN <- Qt$QInputDialog$getDouble
+    ans <- FUN(parent,
+               as.character(title),
+               as.character(label),
+               as.double(value),
+               as.double(minValue),
+               as.double(maxValue),
+               as.integer(decimals))
+    (ans)
+}
+
+qgetInteger <-
+    function(title = "Choose value", label = "Enter an integer value", value = 0L,
+             minValue = -.Machine$integer.max,
+             maxValue = .Machine$integer.max,
+             step = 1L, parent = NULL)
+{
+    stop("Not fixed -- easy")
+    ## .Call(qt_qgetInteger,
+    ##       as.character(title)[1],
+    ##       as.character(label)[1],
+    ##       as.integer(value)[1],
+    ##       as.integer(minValue)[1], as.integer(maxValue)[1],
+    ##       as.integer(step)[1],
+    ##       if (is(parent, "QWidget")) parent else NULL)
+}
+
+qgetText <-
+    function(title = "Choose value", label = "Enter a text string",
+             text = "", parent = NULL)
+{
+    stop("Not fixed -- easy")
+    ## .Call(qt_qgetText,
+    ##       as.character(title)[1],
+    ##       as.character(label)[1],
+    ##       as.character(text)[1], 
+    ##       if (is(parent, "QWidget")) parent else NULL)
+}
+
+
+##
+
+
+qexport <- function(x, ...)
+{
+    stop("Not updated yet")
+    ## UseMethod("qexport")
+}
+
 
 qexport.QWidget <-
     function(x,
@@ -56,57 +126,4 @@ qexport.QGraphicsView <-
 {
     qrenderGraphicsView(x)
 }
-
-
-
-
-qgetColor <- function(parent = NULL)
-{
-    ans <-
-        .Call(qt_qgetColor,
-              if (is(parent, "QWidget")) parent else NULL)
-    rgb(ans[1], ans[2], ans[3], ans[4], maxColorValue = 255)
-}
-
-qgetDouble <-
-    function(title = "Choose value", label = "Enter a numeric value", value = 0.0,
-             minValue = -.Machine$double.xmax,
-             maxValue = .Machine$double.xmax,
-             decimals = 3L, parent = NULL)
-{
-    .Call(qt_qgetDouble,
-          as.character(title)[1],
-          as.character(label)[1],
-          as.double(value)[1],
-          as.double(minValue)[1], as.double(maxValue)[1],
-          as.integer(decimals)[1],
-          if (is(parent, "QWidget")) parent else NULL)
-}
-
-qgetInteger <-
-    function(title = "Choose value", label = "Enter an integer value", value = 0L,
-             minValue = -.Machine$integer.max,
-             maxValue = .Machine$integer.max,
-             step = 1L, parent = NULL)
-{
-    .Call(qt_qgetInteger,
-          as.character(title)[1],
-          as.character(label)[1],
-          as.integer(value)[1],
-          as.integer(minValue)[1], as.integer(maxValue)[1],
-          as.integer(step)[1],
-          if (is(parent, "QWidget")) parent else NULL)
-}
-
-qgetText <-
-    function(title = "Choose value", label = "Enter a text string",
-             text = "", parent = NULL)
-{
-    .Call(qt_qgetText,
-          as.character(title)[1],
-          as.character(label)[1],
-          as.character(text)[1], 
-          if (is(parent, "QWidget")) parent else NULL)
-}
-
 
